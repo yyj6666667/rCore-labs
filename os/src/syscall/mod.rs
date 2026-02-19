8fb74a4bba004ca137e38a2a3b90d86ccae9ccb5
yyj <yyj6666667@gmail.com>
2026-02-19 16:13:09 +0800

    fix: syscall_cnt not increase

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -28,11 +28,15 @@ const SYSCALL_TRACE: usize = 410;
 mod fs;
 mod process;
 
+use crate::task::{get_current_task_id, increase_syscall_cnt};
 use fs::*;
 use process::*;
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+    let task_id = get_current_task_id();
+    increase_syscall_cnt(task_id, syscall_id);
+
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
